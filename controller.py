c0d099df14a1c3c1d7a9f823e5559b023169705d
annaaborgogno <s311319@studenti.polito.it>
2025-03-23 18:49:45 +0100

    Concluso laboratorio 4

--- controller.py
+++ controller.py
@@ -57,6 +57,37 @@ class SpellChecker:
               "______________________________\n")
 
 
+    def handleLanguageSelection(self, e):
+        self._view.txtOut.controls.append(ft.Text(value="Lingua scelta correttamente: " + self._view.ddLanguage.value))
+        self._view.update()
+
+    def handleModalitySelection(self, e):
+        self._view.txtOut.controls.append(ft.Text(value="Modalità scelta correttamente: " + self._view.ddModality.value))
+        self._view.update()
+
+    def handleSpellCheck(self, e):
+        lingua = self._view.ddLanguage.value
+        modalita = self._view.ddModality.value
+        frase = self._view.testo.value
+
+        if lingua == "":
+            self._view.txtOut.controls.append(ft.Text(value="Attenzione, selezionare una lingua", color="red"))
+            self._view.update()
+            return
+        if modalita == "":
+            self._view.txtOut.controls.append(ft.Text(value="Attenzione, selezionare una modalita", color="red"))
+            self._view.update()
+            return
+        if frase == "":
+            self._view.txtOut.controls.append(ft.Text(value="Attenzione, selezionare una frase", color="red"))
+            return
+
+        paroleSbagliate, tempo = self.handleSentence(frase, lingua, modalita)
+
+        self._view.txtOut.controls.append(ft.Text(value=f"Frase inserita: {frase}\n Parole sbagliate: {paroleSbagliate}\n Tempo impiegato: {tempo}"))
+        self._view.update()
+
+
 def replaceChars(text):
     chars = "\\`*_{}[]()>#+-.!$?%^;,=_~"
     for c in chars:
--- view.py
+++ view.py
@@ -1,10 +1,12 @@
 import flet as ft
+from httpx import options
+
 
 class View(object):
     def __init__(self, page: ft.Page):
         # Page
         self.page = page
-        self.page.title = "TdP 2024 - Lab 04 - SpellChecker ++"
+        self.page.title = "TdP 2025 - Lab 04 - SpellChecker ++"
         self.page.horizontal_alignment = 'CENTER'
         self.page.theme_mode = ft.ThemeMode.LIGHT
         # Controller
@@ -12,6 +14,7 @@ class View(object):
         # UI elements
         self.__title = None
         self.__theme_switch = None
+        self.__txtOut = None
 
         # define the UI elements and populate the page
 
@@ -19,7 +22,7 @@ class View(object):
         """Function that creates and adds the visual elements to the page. It also updates
         the page accordingly."""
         # title + theme switch
-        self.__title = ft.Text("TdP 2024 - Lab 04 - SpellChecker ++", size=24, color="blue")
+        self.__title = ft.Text("TdP 2025 - Lab 04 - SpellChecker ++", size=24, color="blue")
         self.__theme_switch = ft.Switch(label="Light theme", on_change=self.theme_changed)
         self.page.controls.append(
             ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
@@ -27,15 +30,29 @@ class View(object):
         )
 
         # Add your stuff here
+        self.ddLanguage = ft.Dropdown(label="Lingue", hint_text="Seleziona lingua",
+                         options=[ft.dropdown.Option("italian"), ft.dropdown.Option("english"), ft.dropdown.Option("spanish")], on_change=self.__controller.handleLanguageSelection)
+        row1 = ft.Row([self.ddLanguage], ft.MainAxisAlignment.CENTER)
+        self.page.update()
 
-        self.page.add([])
+        self.ddModality = ft.Dropdown(label="Modalità", hint_text="Seleziona la modalità di ricerca",
+                         options=[ft.dropdown.Option("Default"), ft.dropdown.Option("Linear"), ft.dropdown.Option("Dichotomic")], on_change=self.__controller.handleModalitySelection)
 
+        self.testo = ft.TextField(label="Inserisci il testo: ")
+
+        self.btnCheck = ft.ElevatedButton(text="Spell check", on_click=self.__controller.handleSpellCheck)
+
+        row2 = ft.Row([self.ddModality, self.testo, self.btnCheck], ft.MainAxisAlignment.CENTER)
+        self.txtOut = ft.ListView(expand=1, spacing=10, padding=20, auto_scroll=True)
+        self.page.add(row1, row2, self.txtOut)
         self.page.update()
 
     def update(self):
         self.page.update()
+
     def setController(self, controller):
         self.__controller = controller
+
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
